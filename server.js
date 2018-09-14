1293205e56187ced3f4d98ddbde9ed7599fbce6a
Unknown <glrd.remi@gmail.com>
2018-09-15 00:57:44 +0200

    add serrver

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const http = require('http');
+
+app.use(express.static(path.join(__dirname, './build')));
+app.use(helmet());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(80, function () {
+    console.log('Server started ... listening on port 80!');
+});
